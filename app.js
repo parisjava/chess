f724fff1abd8f56adb7636715a2ae541448abd2a
Varun Radhakrishnan <vradhak2@jhu.edu>
2017-06-05 22:11:28 -0400

    Allowed the option to choose port

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.port || 3000;
 var inLobby = {};
 
 app.use(express.static('public'));
@@ -31,6 +32,6 @@ io.on('connection', function(socket) {
     
 });
 
-http.listen(3000, function() {
+http.listen(port, function() {
     console.log('listening on *:3000');
 });
